d7b49938be40850bc8d50bd61ad278c57b9245bb
Natalia-gb <ngomezbonilla@gmail.com>
2023-06-12 13:49:17 +0200

    Proyecto terminado

--- src/Helpers/useLocalStorage.js
+++ src/Helpers/useLocalStorage.js
@@ -10,11 +10,11 @@ const getStorageValue = (key, defaultValue) => {
 export const useLocalStorage = (key, defaultValue) => {
     const [value, setValue] = useState(() => {
         return getStorageValue(key, defaultValue);
-    });
+    })
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
+    }, [key, value])
 
     return [value, setValue];
 };
